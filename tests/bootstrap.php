dafda6b37ed5ad9dddfee04a5acf2b31abfaa452
Jacques Marneweck <jacques@powertrip.co.za>
2018-08-04 23:08:23 +0200

    Prep for testing

---
+++ tests/bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Adyen Parsers for PHP
+ *
+ * @author    Jacques Marneweck <jacques@powertrip.co.za>
+ * @copyright 2018 Jacques Marneweck.  All rights strictly reserved.
+ * @license   MPLv2
+ */
+
+require_once __DIR__.'/../vendor/autoload.php';
