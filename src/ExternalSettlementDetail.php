8fc05766eb439a3ba363008c5034243c5313073b
Jacques Marneweck <jacques@powertrip.co.za>
2018-08-15 16:34:27 +0200

    Install moneyphp/money for monetary value parsing

---
+++ src/ExternalSettlementDetail.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @author Jacques Marneweck <jacques@siberia.co.za>
+ * @copyright 2018 Jacques Marneweck.  All rights strictly reserved.
+ */
+
+namespace Jacques\Adyen\Parsers;
+
+class ExternalSettlementDetail
+{
+
+    public function __construct()
+    {
+    }
+
+    public function parse()
+    {
+    }
+}
